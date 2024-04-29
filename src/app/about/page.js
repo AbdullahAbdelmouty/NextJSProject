067868f10fc179808ed2de86a5d048ec32f87ee1
abdallah abdelmoaty <abdallah.abdelmoaty@tasahel.local>
2024-04-29 13:29:20 +0300

    handle tag

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -2,15 +2,14 @@
 import Template from "@/app/Template";
 import { useEffect } from "react";
 import { gtmPageView } from "@/lib/gtm";
-import { usePathname } from 'next/navigation';
+// import { usePathname } from 'next/navigation';
 export default function About({params}) {
-  const pathName = usePathname()
-  console.log(pathName,"path");
+  console.log("path");
   useEffect(()=>{
     const props = {
       page_title: "about"
     }
-  },[pathName])
+  },[])
   return (
     <div>
       <h1>About in Tasahel</h1>
